310d0e4f29c77905922033672f1e932d67ec2ffc
Jun Lee <82641992+Jun3-dev@users.noreply.github.com>
2021-07-14 14:51:46 +1000

    Added movement
    Added movement and controls but not collisions, erros with jumping

--- platformergame.py
+++ platformergame.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
 #define game variables
-tile_size = 33
+tile_size = 33																		
 
 
 #load images
@@ -20,11 +20,58 @@ sky_img = pygame.image.load('C:/img/sky.png')
 def draw_grid():
 	for line in range(0, 20):
 		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), 							(line * tile_size, screen_height))
+
+
+class Player():
+	def __init__(self, x, y):
+		img = pygame.image.load('C:/img/guy1.png')		
+		self.image = pygame.transform.scale(img, (40,80))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
+	
+	def update(self):
+
+		dx = 0
+		dy = 0
+		#get keypresses
+		key = pygame.key.get_pressed()
+		if key [pygame.K_UP] and self.jumped == False:
+			self.vel_y = -15
+			self.jumped = True
+		if key [pygame.K_UP] == False:
+			self.jumped = False
+		if key [pygame.K_LEFT]:
+			dx -= 5
+		if key [pygame.K_RIGHT]:
+			dx += 5
+
+		#add gravity
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y	
+
+		#check colision
+
+		#update player coords
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
 
 
 
-class World():
+
+
+		#draw player onto screen
+		screen.blit(self.image, self.rect)
+
+class World():	
 	def __init__(self, data):
 		self.tile_list = []
 
@@ -69,7 +116,7 @@ world_data = [
 [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 	
 [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
@@ -82,7 +129,7 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-
+player = Player(100, screen_height - 130)
 world = World(world_data)
 
 run = True
@@ -93,7 +140,8 @@ while run:
 
 	world.draw()
 
-	draw_grid()
+	player.update()
+
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:

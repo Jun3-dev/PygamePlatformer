cb3ba2ed759da6a9a1e3ff2339897dd62b0d69c2
Jun Lee <82641992+Jun3-dev@users.noreply.github.com>
2021-07-14 16:03:35 +1000

    update pygameplatformer.py
    Added collisions

--- platformergame.py
+++ platformergame.py
@@ -6,7 +6,6 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 60
 
-
 screen_width = 650
 screen_height = 650
 
@@ -14,28 +13,23 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
 #define game variables
-tile_size = 33																		
+tile_size = 33
 
 
 #load images
 sun_img = pygame.image.load('C:/img/sun.png')
-sky_img = pygame.image.load('C:/img/sky.png')
-
-def draw_grid():
-	for line in range(0, 20):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), 							(line * tile_size, screen_height))
+bg_img = pygame.image.load('C:/img/sky.png')
 
 
 class Player():
 	def __init__(self, x, y):
 		self.images_right = []
-		self.images_left  = []
+		self.images_left = []
 		self.index = 0
 		self.counter = 0
-		for num in range(1,5):
-			img_right = pygame.image.load(f'C:/img/guy{num}.png')		
-			img_right = pygame.transform.scale(img_right, (40,80))
+		for num in range(1, 5):
+			img_right = pygame.image.load(f'C:/img/guy{num}.png')
+			img_right = pygame.transform.scale(img_right, (30, 60))
 			img_left = pygame.transform.flip(img_right, True, False)
 			self.images_right.append(img_right)
 			self.images_left.append(img_left)
@@ -43,74 +37,94 @@ class Player():
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 		self.vel_y = 0
 		self.jumped = False
 		self.direction = 0
-	
-	def update(self):
 
+	def update(self):
 		dx = 0
 		dy = 0
 		walk_cooldown = 5
+
 		#get keypresses
 		key = pygame.key.get_pressed()
-		if key [pygame.K_UP] and self.jumped == False:
+		if key[pygame.K_UP] and self.jumped == False:
 			self.vel_y = -15
 			self.jumped = True
-		if key [pygame.K_UP] == False:
+		if key[pygame.K_UP] == False:
 			self.jumped = False
-		if key [pygame.K_LEFT]:
+		if key[pygame.K_LEFT]:
 			dx -= 5
 			self.counter += 1
 			self.direction = -1
-		if key [pygame.K_RIGHT]:
+		if key[pygame.K_RIGHT]:
 			dx += 5
 			self.counter += 1
 			self.direction = 1
-		if key [pygame.K_LEFT] == False and [pygame.K_RIGHT] == False:
+		if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
 			self.counter = 0
 			self.index = 0
 			if self.direction == 1:
-				self.image = self.images_right[self.index] 
+				self.image = self.images_right[self.index]
 			if self.direction == -1:
-					self.image = self.images_left[self.index] 
+				self.image = self.images_left[self.index]
 
 
-		#handle animations
+		#handle animation
 		if self.counter > walk_cooldown:
-			self.counter = 0
+			self.counter = 0	
 			self.index += 1
 			if self.index >= len(self.images_right):
 				self.index = 0
 			if self.direction == 1:
-				self.image = self.images_right[self.index] 
+				self.image = self.images_right[self.index]
 			if self.direction == -1:
-					self.image = self.images_left[self.index] 
+				self.image = self.images_left[self.index]
 
 
 		#add gravity
 		self.vel_y += 1
 		if self.vel_y > 10:
 			self.vel_y = 10
-		dy += self.vel_y	
+		dy += self.vel_y
 
-		#check colision
+		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
 
-		#update player coords
+
+
+
+		#update player coordinates
 		self.rect.x += dx
 		self.rect.y += dy
 
 		if self.rect.bottom > screen_height:
 			self.rect.bottom = screen_height
-
-
-
-
+			dy = 0
 
 		#draw player onto screen
 		screen.blit(self.image, self.rect)
+		pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
-class World():	
+
+
+
+class World():
 	def __init__(self, data):
 		self.tile_list = []
 
@@ -142,6 +156,7 @@ class World():
 	def draw(self):
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
+			pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
 
@@ -155,7 +170,7 @@ world_data = [
 [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 	
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
@@ -168,6 +183,8 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+
+
 player = Player(100, screen_height - 130)
 world = World(world_data)
 
@@ -176,14 +193,12 @@ while run:
 
 	clock.tick(fps)
 
-
-	screen.blit(sky_img, (0, 0))
+	screen.blit(bg_img, (0, 0))
 	screen.blit(sun_img, (100, 100))
 
 	world.draw()
 
 	player.update()
-
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
